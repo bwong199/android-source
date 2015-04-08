5ee94543f0b1de61452efd6562142996257f7fc3
Ben <bwong199@my.bcit.ca>
2015-04-07 21:57:35 -0600

    Checkpoint 14 assignment completed

--- foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
+++ foundation/12-syntax/src/com/bloc/syntax/SyntaxDisaster.java
@@ -1,24 +1,25 @@
-
 package com.bloc.syntax;
 
 public class SyntaxDisaster extends Object {
-
-	/************************************************
-	 *	ASSIGNMENT:
-	 *	Fix the code found below this block
-	/************************************************/
-
-	public static void main(String [] args) {;}
-		if (true); {
-			int x = 5
-		}
-		int syntaxIsFun; 5;
-		/* The following line prints stuff out, I promise
-		System.out.println("If you can see this message, you've won!);
-	}
-
-	/************************************************
-	 *	ASSIGNMENT:
-	 *	Fix the code found above this block
-	/************************************************/
-}
+    
+    /************************************************
+     *	ASSIGNMENT:
+     *	Fix the code found below this block
+     /************************************************/
+    
+    public static void main(String [] args) {
+        boolean syntaxIsFun;
+        if (syntaxIsFun) {
+        System.out.println("If you can see this message, you've won!");
+        }
+                           }
+    
+        /* The following line prints stuff out, I promise
+         
+         
+         
+         /************************************************
+         *	ASSIGNMENT:
+         *	Fix the code found above this block
+         /************************************************/
+    }
