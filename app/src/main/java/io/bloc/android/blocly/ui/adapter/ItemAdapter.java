2d4243b0c7c3f14891cbdbad10e41f8a9394720f
Ben <bwong199@my.bcit.ca>
2015-04-14 11:09:52 -0600

    initial commit

--- app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
+++ app/src/main/java/io/bloc/android/blocly/ui/adapter/ItemAdapter.java
@@ -1,21 +1,32 @@
 package io.bloc.android.blocly.ui.adapter;
 
+import android.graphics.Bitmap;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingListener;
+
+import io.bloc.android.blocly.BloclyApplication;
 import io.bloc.android.blocly.R;
+import io.bloc.android.blocly.api.DataSource;
 import io.bloc.android.blocly.api.model.RssFeed;
 import io.bloc.android.blocly.api.model.RssItem;
 
 /**
  * Created by benwong on 2015-04-13.
  */
-public class ItemAdapter {
+
     public class ItemAdapter extends RecyclerView.Adapter<ItemAdapter.ItemAdapterViewHolder> {
 
+    private static String TAG = ItemAdapter.class.getSimpleName();
 
         @Override
         public ItemAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int index) {
@@ -37,11 +48,14 @@ public class ItemAdapter {
         }
 
 
-        class ItemAdapterViewHolder extends RecyclerView.ViewHolder {
+    class ItemAdapterViewHolder extends RecyclerView.ViewHolder implements ImageLoadingListener, View.OnClickListener {
 
             TextView title;
             TextView feed;
             TextView content;
+            View headerWrapper;
+            ImageView headerImage;
+            RssItem rssItem;
 
             public ItemAdapterViewHolder(View itemView) {
                 super(itemView);
@@ -49,12 +63,60 @@ public class ItemAdapter {
                 title = (TextView) itemView.findViewById(R.id.tv_rss_item_title);
                 feed = (TextView) itemView.findViewById(R.id.tv_rss_item_feed_title);
                 content = (TextView) itemView.findViewById(R.id.tv_rss_item_content);
+
+                headerWrapper = itemView.findViewById(R.id.fl_rss_item_image_header);
+                headerImage = (ImageView) headerWrapper.findViewById(R.id.iv_rss_item_image);
+
+                itemView.setOnClickListener(this);
             }
 
             void update(RssFeed rssFeed, RssItem rssItem) {
+                this.rssItem = rssItem;
                 feed.setText(rssFeed.getTitle());
                 title.setText(rssItem.getTitle());
                 content.setText(rssItem.getDescription());
+                if (rssItem.getImageUrl() != null) {
+                    headerWrapper.setVisibility(View.VISIBLE);
+                    headerImage.setVisibility(View.INVISIBLE);
+                    ImageLoader.getInstance().loadImage(rssItem.getImageUrl(), this);
+                } else {
+                    headerWrapper.setVisibility(View.GONE);
+                }
             }
+
+        /*
+         * ImageLoadingListener
+         */
+
+            @Override
+            public void onLoadingStarted(String imageUri, View view) {}
+
+            @Override
+            public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                Log.e(TAG, "onLoadingFailed: " + failReason.toString() + " for URL: " + imageUri);
+            }
+
+            @Override
+            public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+// #10
+                if (imageUri.equals(rssItem.getImageUrl())) {
+                    headerImage.setImageBitmap(loadedImage);
+                    headerImage.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @Override
+            public void onLoadingCancelled(String imageUri, View view) {
+                // Attempt a retry
+                ImageLoader.getInstance().loadImage(imageUri, this);
+            }
+
+        /*
+          * OnClickListener
+          */
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), rssItem.getTitle(), Toast.LENGTH_SHORT).show();
         }
+    }
 }
