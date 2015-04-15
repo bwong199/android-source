be3ab1d8923e4406379b475c6222db022a96622e
Ben <bwong199@my.bcit.ca>
2015-04-15 09:47:45 -0600

    Checkpoint 38 assignment completed

--- app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
+++ app/src/main/java/io/bloc/android/blocly/ui/activity/BloclyActivity.java
@@ -1,19 +1,43 @@
 package io.bloc.android.blocly.ui.activity;
 
 import android.app.Activity;
-import android.os.Bundle;
 
-import io.bloc.android.blocly.R;
+import com.nostra13.universalimageloader.cache.memory.impl.LruMemoryCache;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+import javax.sql.DataSource;
 
 /**
  * Created by benwong on 2015-04-13.
  */
-public class BloclyActivity {
+
     public class BloclyActivity extends Activity {
-        @Override
-        protected void onCreate(Bundle savedInstanceState){
-            super.onCreate((savedInstanceState));
-            setContentView(R.layout.activity_blocly);
-        }
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sharedInstance = this;
+        dataSource = new DataSource();
+
+// #1
+        DisplayImageOptions defaultOptions = new DisplayImageOptions.Builder()
+                .cacheOnDisk(true)
+                .cacheInMemory(true)
+                .build();
+
+// #2
+        ImageLoaderConfiguration configuration = new ImageLoaderConfiguration.Builder(this)
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .denyCacheImageMultipleSizesInMemory()
+                .memoryCache(new LruMemoryCache(2 * 1024 * 1024))
+                .memoryCacheSize(2 * 1024 * 1024)
+                .diskCacheSize(50 * 1024 * 1024)
+                .diskCacheFileCount(100)
+                .defaultDisplayImageOptions(defaultOptions)
+                .build();
+
+        ImageLoader.getInstance().init(configuration);
     }
 }
