505422eef49e63f6f91b6f3d21c39d22334d558a
Ben <bwong199@my.bcit.ca>
2015-04-16 23:03:15 -0600

    Checkpoint 31 completed

--- foundation/31-threads/src/com/bloc/threads/ImageGetter.java
+++ foundation/31-threads/src/com/bloc/threads/ImageGetter.java
@@ -30,6 +30,7 @@ public class ImageGetter extends Thread {
 
 	@Override
 	public void run() {
+        
 		/************************************************
  		 *	ASSIGNMENT:
  		 *	Perform the work found in `Main.java` in this
